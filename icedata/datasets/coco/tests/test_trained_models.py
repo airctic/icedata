9ca060e298797a60388af4f2ee800984e4d74762
lgvaz <lgvaz42@gmail.com>
2020-09-14 20:06:28 -0300

    asserts trained_model return type on tests

--- icedata/datasets/coco/tests/test_trained_models.py
+++ icedata/datasets/coco/tests/test_trained_models.py
@@ -1,8 +1,10 @@
 import pytest
 import icedata
+import torch.nn as nn
 
 
 @pytest.mark.parametrize("model_name", icedata.coco.trained_models.__all__)
 def test_trained_models(model_name):
     model_fn = getattr(icedata.coco.trained_models, model_name)
     model = model_fn()
+    assert isinstance(model, nn.Module)
--- icedata/datasets/pennfudan/tests/test_trained_models.py
+++ icedata/datasets/pennfudan/tests/test_trained_models.py
@@ -1,8 +1,10 @@
 import pytest
 import icedata
+import torch.nn as nn
 
 
 @pytest.mark.parametrize("model_name", icedata.pennfudan.trained_models.__all__)
-def test_trained_models(model_name):
+def test_trained_models(model_name) -> nn.Module:
     model_fn = getattr(icedata.pennfudan.trained_models, model_name)
     model = model_fn()
+    assert isinstance(model, nn.Module)
