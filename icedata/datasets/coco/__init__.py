86181cad8ef56c42698290d6c36d7099f520ea6c
lgvaz <lgvaz42@gmail.com>
2020-09-14 15:27:19 -0300

    adds coco dataset

---
+++ icedata/datasets/coco/__init__.py
@@ -0,0 +1,3 @@
+from icedata.datasets.coco.data import *
+from icedata.datasets.coco.parsers import *
+from icedata.datasets.coco import trained_models
--- icedata/datasets/coco/data.py
+++ icedata/datasets/coco/data.py
@@ -1,10 +1,117 @@
-__all__ = ["class_map"]
+__all__ = ["NUM_CLASSES", "class_map", "load_data"]
 
 from icevision.imports import *
 from icevision.core import *
 
+_CLASSES = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "N/A",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "N/A",
+    "backpack",
+    "umbrella",
+    "N/A",
+    "N/A",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "N/A",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "N/A",
+    "dining table",
+    "N/A",
+    "N/A",
+    "toilet",
+    "N/A",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "N/A",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
 
-def class_map(annotations_file, background: Optional[int] = 0) -> ClassMap:
-    annotations_dict = json.loads(annotations_file.read())
-    classes = [cat["name"] for cat in annotations_dict["categories"]]
-    return ClassMap(classes=classes, background=background)
+NUM_CLASSES = len(_CLASSES) + 1
+
+
+def class_map(background: Optional[int] = 0) -> ClassMap:
+    return ClassMap(classes=_CLASSES, background=background)
+
+
+def load_data():
+    raise NotImplementedError(
+        "Downloading data for COCO is not implemented, please consider following the "
+        "official instructions avaiable [here](https://cocodataset.org/#download) "
+        "to download the dataset"
+    )
+
+
+# def class_map(annotations_file, background: Optional[int] = 0) -> ClassMap:
+#     annotations_dict = json.loads(annotations_file.read())
+#     classes = [cat["name"] for cat in annotations_dict["categories"]]
+#     return ClassMap(classes=classes, background=background)
