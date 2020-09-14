bef56b9c41bbd6eb308443890346dadfea06b69e
lgvaz <lgvaz42@gmail.com>
2020-09-14 20:05:21 -0300

    fixes returning model for coco

--- icedata/datasets/coco/trained_models.py
+++ icedata/datasets/coco/trained_models.py
@@ -5,9 +5,10 @@ from icedata.utils import load_model_weights_from_url
 from icedata.datasets.coco.data import NUM_CLASSES
 
 
-def faster_rcnn_resnet50_fpn():
+def faster_rcnn_resnet50_fpn() -> nn.Module:
     weights_url = (
         "https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
     )
     model = faster_rcnn.model(num_classes=NUM_CLASSES)
     load_model_weights_from_url(model=model, url=weights_url)
+    return model
