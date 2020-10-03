24f30200bbe1c36f78181088ed00f3179c610dbd
Aadhav Vignesh <53528139+aadhav-n1@users.noreply.github.com>
2020-10-03 07:12:45 +0530

    Refactored icedata dataset parsers (#53)

--- icedata/datasets/pennfudan/__init__.py
+++ icedata/datasets/pennfudan/__init__.py
@@ -1,3 +1,3 @@
 from icedata.datasets.pennfudan.data import *
-from icedata.datasets.pennfudan.parsers import *
+from icedata.datasets.pennfudan.parser import *
 from icedata.datasets.pennfudan import trained_models
