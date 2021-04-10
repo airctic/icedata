f1fee98bb16cbd18fba60dc70a0cf6059468fefb
lgvaz <lgvaz42@gmail.com>
2021-04-10 10:02:53 -0300

    adds biwi dataset

--- icedata/datasets/biwi/parser.py
+++ icedata/datasets/biwi/parser.py
@@ -1,6 +1,7 @@
 __all__ = ["parser"]
 
 from icevision.all import *
+from icevision.core.record_defaults import KeypointsRecord
 
 
 def parser(data_dir: Path):
@@ -15,10 +16,11 @@ class BIWIKeypointsMetadata(KeypointsMetadata):
 
 
 class BIWIParser(Parser):
-    def __init__(self, annotations_filepath, img_dir):
-        raise NotImplementedError("Has to be refactored to new API")
+    def __init__(self, annotations_filepath, img_dir, idmap=None):
+        super().__init__(template_record=self.template_record(), idmap=idmap)
         self.annotations_dict = pickle.load(open(Path(annotations_filepath), "rb"))
         self.img_dir = Path(img_dir)
+        self.class_map = ClassMap(["nose"])
 
     def __iter__(self):
         yield from self.annotations_dict.items()
@@ -26,7 +28,10 @@ class BIWIParser(Parser):
     def __len__(self):
         return len(self.annotations_dict)
 
-    def imageid(self, o):
+    def template_record(self) -> BaseRecord:
+        return KeypointsRecord()
+
+    def record_id(self, o):
         return int(o[0].replace(".jpg", ""))
 
     def filepath(self, o):
@@ -37,11 +42,21 @@ class BIWIParser(Parser):
         return [KeyPoints.from_xyv([x, y, 1], BIWIKeypointsMetadata)]
 
     def image_width_height(self, o) -> Tuple[int, int]:
-        return get_image_size(self.filepath(o))
+        return get_img_size(self.filepath(o))
 
     def labels(self, o) -> List[Hashable]:
         return [1]
 
     def bboxes(self, o) -> List[BBox]:
-        w, h = get_image_size(self.filepath(o))
+        w, h = get_img_size(self.filepath(o))
         return [BBox.from_xywh(0, 0, w, h)]
+
+    def parse_fields(self, o, record, is_new):
+        if is_new:
+            record.set_filepath(self.filepath(o))
+            record.set_img_size(self.image_width_height(o))
+
+        record.detection.set_class_map(self.class_map)
+        record.detection.add_labels_by_id(self.labels(o))
+        record.detection.add_bboxes(self.bboxes(o))
+        record.detection.add_keypoints(self.keypoints(o))
