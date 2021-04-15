4e8eb37a4924dafd9b97627dad2d3c43394be7a7
lgvaz <lgvaz42@gmail.com>
2021-04-15 10:24:22 -0300

    updates birds dataset

--- icedata/datasets/birds/__init__.py
+++ icedata/datasets/birds/__init__.py
@@ -1,3 +1,4 @@
 from icedata.datasets.birds.data import *
 from icedata.datasets.birds.parser import *
+from icedata.datasets.birds.dataset import *
 from icedata.datasets.birds import trained_models
